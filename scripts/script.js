5922c3f5481201fe54216635541c462dbc0a37b4
ThomasLeMah <129088785+ThomasLeMah@users.noreply.github.com>
2023-07-26 19:40:22 +0200

    script.js

--- scripts/script.js
+++ scripts/script.js
@@ -56,6 +56,74 @@ function customIcon(icon){
 	});
 }
 
-var X  = 0;
-var Y = 0;
-L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("I am here.");
+var X  = 1210;
+var Y = -3150;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Laboratoire");
+
+var X  = 1370;
+var Y = -2100;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Atelier d'armes");
+
+var X  = 1650;
+var Y = -2850;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Épave d'avion (Kérosene - T-Shirts Gucci)");
+
+var X  = -1380;
+var Y = -660;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Print");
+
+var X  = -1525;
+var Y = -440;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Vol de vêtements de luxe");
+
+var X  = -2725;
+var Y = -680;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Épave batiment (Nitrogène - Sulfure)");
+
+var X  = 3250;
+var Y = -500;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Épave bateau (Matériel MerryWeather - Balles 9mm, .45acp)");
+
+var X  = 3550;
+var Y = 3400;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Épave bateau (Récolte Weed)");
+
+var X  = 3550;
+var Y = 3700;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Braquage entrepôts (explosifs)");
+
+var X  = 2190;
+var Y = 3320;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Informations (argent sale)");
+
+var X  = 2350;
+var Y = 3040;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Achat de métaux");
+
+var X  = -250;
+var Y = 2940;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Récolte cocaïne");
+
+var X  = -95;
+var Y = 2810;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Petits boulots");
+
+var X  = -3550;
+var Y = 3500;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Épave d'avion (loot inconnu)");
+
+var X  = -810;
+var Y = 4300;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Coffre argent");
+
+var X  = 3700;
+var Y = 4900;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Cabane îlot (coffre argent + hélico)");
+
+var X  = 2205;
+var Y = 5610;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Récolte codéine");
+
+var X  = 2755;
+var Y = 6610;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("Épave sous-marin (Explosifs - Masques à gaz)");
